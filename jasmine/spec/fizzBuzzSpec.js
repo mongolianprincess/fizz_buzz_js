55e0c93bf3336f749c37a10155784a75840a0cbd
hasulica <raduasandei@gmail.com>
2015-11-10 11:53:56 +0000

    Initial commit

---
+++ jasmine/spec/fizzBuzzSpec.js
@@ -0,0 +1,13 @@
+describe('fizzBuzz', function() {
+  var fizzbuzz;
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 3', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+  });
+
+});
