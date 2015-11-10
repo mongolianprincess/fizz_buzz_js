0cb0cfc1436047da31ad660d6a97eea908311767
mongolianprincess <ayc.yang@gmail.com>
2015-11-10 12:17:52 +0000

    Added test and method for .isDivisibleByFive

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -8,6 +8,11 @@ describe('fizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('dividisble by 5', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
   });
 
   describe('knows when a number is NOT', function() {
@@ -15,6 +20,11 @@ describe('fizzBuzz', function() {
     it('divisible by 3', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(2)).toBe(false);
     });
 
   });
